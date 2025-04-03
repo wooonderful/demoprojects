0e86065c8b5094561140c6bcc2e596677bea7cd7
shixiangw <shixiangw@foxmail.com>
2025-04-03 13:40:01 +0800

    Update install_for_all_platform.sh

--- file-server/install_for_all_platform.sh
+++ file-server/install_for_all_platform.sh
@@ -2,7 +2,7 @@
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o fileserver_linux  main.go
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o fileserver_windows.exe main.go
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o fileserver_darwin main.go
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o fileserver_darwin main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o fileserver_darwin_amd64 main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o fileserver_darwin_arm64 main.go
 
 CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o fileserver_linux_arm main.go
